126822d98bc97fcb87dc61535c58006eae8eac67
MartinVoelkl94 <martinv16248@gmail.com>
2025-12-16 17:59:23 +0100

    qp.hide: can now be restricted to specific sheets
    Changes to be committed:
    	modified:   qplib/excel.py

--- qplib/excel.py
+++ qplib/excel.py
@@ -9,6 +9,7 @@ def hide(
         patterns=None,
         regex=True,
         hide=True,  #True=hide, False=unhide
+        sheet=None,
         verbosity=3,
         ):  #pragma: no cover (does not affect reading of xlsx files)
     """
@@ -24,34 +25,36 @@ def hide(
 
     if axis in ['col', 'column', 'cols', 'columns', 1]:
         for ws in wb.worksheets:
-            hidden = []
-            for col in ws.columns:
-                if match(patterns, col[0].value, regex):
-                    ws.column_dimensions[col[0].column_letter].hidden = hide
-                    hidden.append(col[0].value)
-            if hidden:
-                hidden = '\n'.join(hidden)
-                text = (
-                    f'debug: columns made {mode} in'
-                    f' "{filename}" sheet "{ws.title}":\n{hidden}'
-                    )
-                log(text, 'qp.hide()', verbosity)
+            if sheet is None or ws.title == sheet:
+                hidden = []
+                for col in ws.columns:
+                    if match(patterns, col[0].value, regex):
+                        ws.column_dimensions[col[0].column_letter].hidden = hide
+                        hidden.append(col[0].value)
+                if hidden:
+                    hidden = '\n'.join(hidden)
+                    text = (
+                        f'debug: columns made {mode} in'
+                        f' "{filename}" sheet "{ws.title}":\n{hidden}'
+                        )
+                    log(text, 'qp.hide()', verbosity)
 
 
     elif axis in ['row', 'rows', 'index', 0]:
         for ws in wb.worksheets:
-            hidden = []
-            for row in ws.rows:
-                if match(patterns, row[0].value, regex):
-                    ws.row_dimensions[row[0].row].hidden = hide
-                    hidden.append(row[0].value)
-            if hidden:
-                hidden = '\n'.join(hidden)
-                text = (
-                    f'debug: rows made {mode} in'
-                    f' "{filename}" sheet "{ws.title}":\n{hidden}'
-                    )
-                log(text, 'qp.hide()', verbosity)
+            if sheet is None or ws.title == sheet:
+                hidden = []
+                for row in ws.rows:
+                    if match(patterns, row[0].value, regex):
+                        ws.row_dimensions[row[0].row].hidden = hide
+                        hidden.append(row[0].value)
+                if hidden:
+                    hidden = '\n'.join(hidden)
+                    text = (
+                        f'debug: rows made {mode} in'
+                        f' "{filename}" sheet "{ws.title}":\n{hidden}'
+                        )
+                    log(text, 'qp.hide()', verbosity)
 
 
     elif axis in ['sheet', 'worksheet', 'tab', 2]:
